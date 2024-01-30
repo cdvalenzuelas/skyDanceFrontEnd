60cd3bf1d0779a566b1ff6a8764ff94d78e348ec
cdvalenzuelas <cdvalenzuelas@gmail.com>
2024-01-30 15:52:22 -0500

    FIRST DEPLOY: FIX NODE_ENV 2

--- src/api/logIn.ts
+++ src/api/logIn.ts
@@ -5,14 +5,16 @@ const supabase = createClientComponentClient()
 
 // Función para iniciar seción
 export const logIn = async () => {
+  const host: string = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : window.location.hostname
+
+  console.log(host)
+
   try {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: {
-        redirectTo: process.env.NODE_ENV === 'production'
-          ? process.env.NEXT_PUBLIC_API_URL + '/auth/callback'
-          : 'http://localhost:3000/auth/callback'
-      }
+      options: { redirectTo: host + '/auth/callback' }
     })
   } catch (error) {
     return []
